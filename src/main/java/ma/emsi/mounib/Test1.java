771ea67f38b9f1453e75c58415de1d4d04682b92
Ghitamounib <ghitamounib9@gmail.com>
2025-11-03 03:07:41 +0100

    Test1 fonctionnel

---
+++ src/main/java/ma/emsi/mounib/Test1.java
@@ -0,0 +1,21 @@
+package ma.emsi.mounib;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        String cle = System.getenv("GEMINI_KEY");
+        ChatModel modele = GoogleAiGeminiChatModel.builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .build();
+
+        String reponse = modele.chat("Superficie de la France ?");
+        System.out.println(reponse);
+    }
+}
+
